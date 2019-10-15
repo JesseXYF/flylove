91de00aaf01666bc695694510c208778c0cfbf4f
xueynfei <528749126@qq.com>
2019-10-15 11:56:19 +0800

    初始化项目

---
+++ src/main/java/com/jesse/flylove/FlyloveApplication.java
@@ -0,0 +1,13 @@
+package com.jesse.flylove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyloveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyloveApplication.class, args);
+    }
+
+}
