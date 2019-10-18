11d5fdc2c8b0fc8920d50e3cbcf0ada95e58a769
xueynfei <528749126@qq.com>
2019-10-18 12:38:15 +0800

    修改页面文字，并从后台获取数据

--- src/main/java/com/jesse/flylove/controller/HtmlController.java
+++ src/main/java/com/jesse/flylove/controller/HtmlController.java
@@ -1,8 +1,14 @@
 package com.jesse.flylove.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @program: CloudEnglish
@@ -14,7 +20,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HtmlController {
 
     @RequestMapping(value = "/photoDetail",method = RequestMethod.GET)
-    public String getShowDetail(){
+    public String getShowDetail(@RequestParam("num") int num, Model model){
+        try {
+            File directory = new File("");//参数为空
+            String courseFile = directory.getCanonicalPath() ;
+            FileReader reader = new FileReader(courseFile+"\\src\\main\\java\\com\\jesse\\flylove\\dto\\photos.txt");
+            BufferedReader buf = new BufferedReader(reader);
+            String s;
+            String textValue = "";
+            int i = 1;
+            while((s = buf.readLine() ) != null){
+                if (i ==num){
+                    textValue = s;
+                    break;
+                }
+                i++;
+            }
+            reader.close();
+            buf.close();
+            model.addAttribute("photoText",textValue);
+        }catch (IOException e){
+            System.out.println(e);
+            model.addAttribute("photoText","一生一世的爱");
+        }
         return "photo_detail";
     }
 }
